lpechacek/quickbox

commit bb53c63569e68d1e018fe79451c78f1b62b5cb8d

QE: Receipt selector

quickevent/app/plugins/qml/Receipts/Receipts.pro
@@ -24,8 +24,9 @@ include (src/src.pri)
 RESOURCES += \
     $${PLUGIN_NAME}.qrc \
 
-DISTFILES += \
-    qml/reports/receiptClassic.qml \
+OTHER_FILES += \
+    qml/reports/receipts/Classic.qml \
+    qml/reports/receipts/LoteryTicket.qml \
     qml/reports/sicard.qml \
 
 TRANSLATIONS += \

libqf/libqfcore/src/core/logdevice.cpp
@@ -628,7 +628,7 @@ void FileLogDevice::log(Log::Level level, const QMessageLogContext &context, con
 	}
 #endif
 
-	std::fprintf(m_file, "|%c", Log::levelName(level)[0]);
+	std::fprintf(m_file, "|%c|", Log::levelName(level)[0]);
 	QString module = moduleFromFileName(context.file);
 	if(!module.isEmpty()) {
 		std::fprintf(m_file, "[%s:%d]", qPrintable(module), context.line);

quickevent/app/plugins/qml/Receipts/qml/reports/receipts/Classic.qml
@@ -3,10 +3,12 @@ import qf.qmlreports 1.0
 import shared.QuickEvent.reports 1.0
 import "qrc:/qf/core/qml/js/timeext.js" as TimeExt
 import "qrc:/quickevent/js/ogtime.js" as OGTime
+import "private" as Private
 
 Report {
 	id: root
 	property int courseLength: 0
+	property bool printLotteryTicket: false
 	//debugLevel: 1
 	styleSheet: StyleSheet {
 		basedOn: ReportStyleCommon {
@@ -47,8 +49,8 @@ Report {
 				width: "%"
 				layout: Frame.LayoutVertical
 				bottomBorder: Pen { basedOn: "black2" }
-				LotteryTicket {
-					visible: false
+				Private.LotteryTicket {
+					visible: printLotteryTicket
 				}
 				Frame {
 					htmlExportAttributes: {"lpt_textAlign": "left", "lpt_borderTop": "=", "lpt_borderBottom": "-"}

quickevent/app/plugins/qml/Receipts/qml/reports/receipts/ClassicLottery.qml
@@ -1,0 +1,7 @@
+//import QtQml 2.0
+
+Classic {
+	printLotteryTicket: true
+}
+
+

quickevent/app/plugins/qml/Receipts/src/Receipts/receiptsplugin.cpp
@@ -64,6 +64,23 @@ Event::EventPlugin *ReceiptsPlugin::eventPlugin()
 	return ret;
 }
 
+QString ReceiptsPlugin::currentReceiptPath()
+{
+	QString printer_options_key = ReceiptsPlugin::SETTINGS_PREFIX;
+	printer_options_key += "receipts/current";
+	qf::core::utils::Settings settings;
+	QString s = settings.value(printer_options_key).toString();
+	return s;
+}
+
+void ReceiptsPlugin::setCurrentReceiptPath(const QString &path)
+{
+	QString printer_options_key = ReceiptsPlugin::SETTINGS_PREFIX;
+	printer_options_key += "receipts/current";
+	qf::core::utils::Settings settings;
+	settings.setValue(printer_options_key, path);
+}
+
 ReceiptsPrinterOptions ReceiptsPlugin::receiptsPrinterOptions()
 {
 	QString printer_options_key = ReceiptsPlugin::SETTINGS_PREFIX;
@@ -354,14 +371,14 @@ void ReceiptsPlugin::previewCard(int card_id)
 void ReceiptsPlugin::previewReceipt(int card_id)
 {
 	//QMetaObject::invokeMethod(this, "previewReceipeClassic", Qt::DirectConnection, Q_ARG(QVariant, card_id));
-	previewReceipt_classic(card_id);
+	previewReceipt(card_id, currentReceiptPath());
 }
 
 bool ReceiptsPlugin::printReceipt(int card_id)
 {
 	QF_TIME_SCOPE("ReceiptsPlugin::printReceipt()");
 	try {
-		printReceipt_classic(card_id);
+		printReceipt(card_id, currentReceiptPath());
 		return true;
 	}
 	catch(const qf::core::Exception &e) {
@@ -385,14 +402,14 @@ bool ReceiptsPlugin::printCard(int card_id)
 	return false;
 }
 
-void ReceiptsPlugin::previewReceipt_classic(int card_id)
+void ReceiptsPlugin::previewReceipt(int card_id, const QString &receipt_path)
 {
 	qfLogFuncFrame() << "card id:" << card_id;
 	//qfInfo() << "previewReceipe_classic, card id:" << card_id;
 	auto *w = new qf::qmlwidgets::reports::ReportViewWidget();
 	w->setPersistentSettingsId("cardPreview");
 	w->setWindowTitle(tr("Receipt"));
-	w->setReport(manifest()->homeDir() + "/reports/receiptClassic.qml");
+	w->setReport(receipt_path);
 	QVariantMap dt = receiptTablesData(card_id);
 	for(auto key : dt.keys())
 		w->setTableData(key, dt.value(key));
@@ -402,11 +419,11 @@ void ReceiptsPlugin::previewReceipt_classic(int card_id)
 	dlg.exec();
 }
 
-void ReceiptsPlugin::printReceipt_classic(int card_id)
+void ReceiptsPlugin::printReceipt(int card_id, const QString &receipt_path)
 {
 	qfLogFuncFrame() << "card id:" << card_id;
 	QVariantMap dt = receiptTablesData(card_id);
-	receiptsPrinter()->printReceipt(manifest()->homeDir() + "/reports/receiptClassic.qml", dt);
+	receiptsPrinter()->printReceipt(receipt_path, dt);
 }
 
 }

quickevent/app/plugins/qml/Receipts/src/Receipts/receiptsplugin.h
@@ -40,6 +40,9 @@ public:
 	QVariantMap readCardTablesData(int card_id);
 	Q_INVOKABLE QVariantMap receiptTablesData(int card_id);
 
+	QString currentReceiptPath();
+	void setCurrentReceiptPath(const QString &path);
+
 	void setReceiptsPrinterOptions(const ReceiptsPrinterOptions &opts);
 	ReceiptsPrinter* receiptsPrinter();
 private:
@@ -49,8 +52,8 @@ private:
 
 	ReceiptsPrinterOptions receiptsPrinterOptions();
 
-	void previewReceipt_classic(int card_id);
-	void printReceipt_classic(int card_id);
+	void previewReceipt(int card_id, const QString &receipt_path);
+	void printReceipt(int card_id, const QString &receipt_path);
 	QList<QByteArray> createPrinterData(const QDomElement &body, const ReceiptsPrinterOptions &printer_options);
 
 	class DirectPrintContext;

quickevent/app/plugins/qml/Receipts/src/receiptswidget.cpp
@@ -38,6 +38,7 @@
 #include <QJSValue>
 #include <QPrinterInfo>
 #include <QTimer>
+#include <QDirIterator>
 
 namespace qfm = qf::core::model;
 namespace qfs = qf::core::sql;
@@ -88,6 +89,10 @@ ReceiptsWidget::ReceiptsWidget(QWidget *parent) :
 	ui->tblCards->setContextMenuPolicy(Qt::CustomContextMenu);
 	connect(ui->tblCards, &qfw::TableView::customContextMenuRequested, this, &ReceiptsWidget::onCustomContextMenuRequest);
 
+	connect(ui->lstReceipt, static_cast<void (QComboBox::*)(int)>(&QComboBox::activated), [this](int ix) {
+		receiptsPlugin()->setCurrentReceiptPath(ui->lstReceipt->itemData(ix).toString());
+	});
+
 	QTimer::singleShot(0, this, &ReceiptsWidget::lazyInit);
 }
 
@@ -98,6 +103,7 @@ ReceiptsWidget::~ReceiptsWidget()
 
 void ReceiptsWidget::lazyInit()
 {
+	loadReceptList();
 	updateReceiptsPrinterLabel();
 }
 
@@ -265,6 +271,34 @@ bool ReceiptsWidget::printReceipt(int card_id)
 		}
 	}
 	return false;
+}
+
+void ReceiptsWidget::loadReceptList()
+{
+	ui->lstReceipt->clear();
+	QString receipts_dir = receiptsPlugin()->manifest()->homeDir() + "/reports/receipts";
+	QDirIterator it(receipts_dir, QStringList{"*.qml"}, QDir::Files | QDir::Readable, QDirIterator::Subdirectories);
+	while (it.hasNext()) {
+		it.next();
+		QFileInfo fi = it.fileInfo();
+		QString path = fi.filePath();
+		QString name = path.mid(receipts_dir.length() + 1);
+		if(name.startsWith("private/", Qt::CaseInsensitive))
+			continue;
+		name = name.mid(0, name.indexOf(".qml", Qt::CaseInsensitive));
+		ui->lstReceipt->addItem(name, path);
+	}
+	QString curr_path = receiptsPlugin()->currentReceiptPath();
+	for (int i = 0; i < ui->lstReceipt->count(); ++i) {
+		if(ui->lstReceipt->itemData(i).toString() == curr_path) {
+			ui->lstReceipt->setCurrentIndex(i);
+			break;
+		}
+	}
+	if(ui->lstReceipt->currentIndex() < 0) {
+		ui->lstReceipt->setCurrentIndex(0);
+		receiptsPlugin()->setCurrentReceiptPath(ui->lstReceipt->itemData(0).toString());
+	}
 }
 
 void ReceiptsWidget::updateReceiptsPrinterLabel()

commit 2d1d70061646feff4a6480b19769df7e0aeaca09

QE: Fix lottery ticket script paths
Fixes: bb53c63569e6 ("QE: Receipt selector")

quickevent/app/plugins/qml/Receipts/Receipts.pro
@@ -26,7 +26,8 @@ RESOURCES += \
 
 OTHER_FILES += \
     qml/reports/receipts/Classic.qml \
-    qml/reports/receipts/LoteryTicket.qml \
+    qml/reports/receipts/ClassicLottery.qml \
+    qml/reports/receipts/private/LotteryTicket.qml \
     qml/reports/sicard.qml \
 
 TRANSLATIONS += \